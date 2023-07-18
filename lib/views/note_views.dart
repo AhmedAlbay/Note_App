d3792e09a77e6fd414fd50f94154d1551d33e119
Your Name <you@example.com>
2023-07-18 18:47:51 +0300

    Update Custom AppBar

--- lib/views/note_views.dart
+++ lib/views/note_views.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/note_view_body.dart';
+
 class NoteViews extends StatelessWidget {
   const NoteViews({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body:NoteViewBody(
+        
+      ) ,
+    );
   }
 }
