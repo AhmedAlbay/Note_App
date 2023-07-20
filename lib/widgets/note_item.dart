4c355843cfb7290136eddd685eda79f0f56ee683
Your Name <you@example.com>
2023-07-20 20:33:44 +0300

    Create Edit Note View

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -1,55 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:note_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCD7B),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16, bottom: 16),
-              child: Text(
-                'Build your Career with Ahmed Mohamed',
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+        }));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCD7B),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
                 style: TextStyle(
-                  color: Colors.black.withOpacity(.5),
-                  fontSize: 18,
+                  color: Colors.black,
+                  fontSize: 26,
                 ),
               ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child: Text(
+                  'Build your Career with Ahmed Mohamed',
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(.5),
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    FontAwesomeIcons.trash,
+                    color: Colors.black,
+                    size: 30,
+                  )),
             ),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  FontAwesomeIcons.trash,
-                  color: Colors.black,
-                  size: 30,
-                )),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24.0),
-            child: Text(
-              'May21,2023',
-              style:
-                  TextStyle(color: Colors.black.withOpacity(.5), fontSize: 16),
+            Padding(
+              padding: const EdgeInsets.only(right: 24.0),
+              child: Text(
+                'May21,2023',
+                style: TextStyle(
+                    color: Colors.black.withOpacity(.5), fontSize: 16),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
