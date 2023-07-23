aae6ac30a30ea180ad86e6695770504a48493b9d
Your Name <you@example.com>
2023-07-23 18:50:53 +0300

    create cubit add note

---
+++ lib/cubits/add_note_cubits.dart/add_note_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+// ignore: depend_on_referenced_packages
+import 'package:meta/meta.dart';
+
+part 'add_note_state.dart';
+
+class AddNoteCubit extends Cubit<AddNoteState> {
+  AddNoteCubit() : super(AddNoteInitial());
+}
