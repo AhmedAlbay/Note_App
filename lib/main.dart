229ff130f984b050647b85e27393da86beb4e42b
Your Name <you@example.com>
2023-07-24 03:44:42 +0300

    Create SimpleBlocObserver

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,13 @@ import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/const.dart';
 import 'package:note_app/cubits/add_note_cubits.dart/add_note_cubit.dart';
 import 'package:note_app/model/note_model.dart';
+import 'package:note_app/simple_bloc_observer.dart';
 import 'package:note_app/views/note_views.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(KNoteBox);
+  Bloc.observer = SimpleBlocObserver();
   Hive.registerAdapter(NoteModelAdapter());
   runApp(const MyApp());
 }
