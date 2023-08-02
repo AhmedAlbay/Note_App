e267851eec1fd7c42f09b85d9fb9d788dabe07f0
Your Name <you@example.com>
2023-08-02 13:52:48 +0300

    delete note

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -4,7 +4,7 @@ import 'package:note_app/model/note_model.dart';
 import 'package:note_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
-  const NoteItem({super.key,  required this.note});
+  const NoteItem({super.key, required this.note});
   final NoteModel note;
   @override
   Widget build(BuildContext context) {
@@ -17,14 +17,14 @@ class NoteItem extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
         decoration: BoxDecoration(
-          color:  Color(note.color),
+          color: Color(note.color),
           borderRadius: BorderRadius.circular(16),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             ListTile(
-              title:  Text(
+              title: Text(
                 note.title,
                 style: const TextStyle(
                   color: Colors.black,
@@ -42,7 +42,9 @@ class NoteItem extends StatelessWidget {
                 ),
               ),
               trailing: IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    note.delete();
+                  },
                   icon: const Icon(
                     FontAwesomeIcons.trash,
                     color: Colors.black,
